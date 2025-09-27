3e691afee2a9ab56ae113f263c98f4ae56dad52d
muhammadzaheer123 <157617139+muhammadzaheer123@users.noreply.github.com>
2025-09-28 00:37:08 +0500

    check

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:5000/api/v1";
-const API_URL = "https://auth-back-henna.vercel.app/api/v1";
+const API_URL = "https://auth-back-henna.vercel.app//api/v1";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
-  },
+  },  
 });
 
 export const authService = {
